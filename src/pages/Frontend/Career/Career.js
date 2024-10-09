d16cd6379773afc07f9826b153845b37af002b9b
Abdul Rafay <abdrafay345@gmail.com>
2024-10-09 22:15:50 +0500

    first commit

---
+++ src/pages/Frontend/Career/Career.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import bannerImg from '../../../assets/images/page-banner.jpg';
+import { Typography } from 'antd';
+
+const { Title } = Typography
+
+export default function Career() {
+    return (
+        <>
+
+            <div className="page__banner" style={{ backgroundImage: `url(${bannerImg})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '150px' }}    >
+                <div className="container-fluid">
+                    <div className="row mb-5">
+                        <div className="col-xl-12 col-lg-12">
+                            <div className="d-flex justify-content-center align-items-center" style={{ paddingTop: "3rem" }}>
+                                <Title level={2} className='text-white'>Careers</Title>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="news__details section-padding">
+                <div className="container">
+                    <div className="row justify-content-center" style={{ marginTop: '70px', marginBottom: '70px' }}>
+                        <div className="col-xl-12">
+                            <div className="news__details-left rounded-2 p-5 mb-4" style={{ backgroundColor: 'rgba(235,235,235,.5)' }}>
+                                <div className="news__details-left-meta">
+                                    <h3 className="mt-20">ANALYST DEVELOPER</h3>
+                                </div>
+                                <p><strong>Job Title :</strong> Analyst Developer</p>
+                                <p><strong>Salary :</strong> €35,000 - 40,000 Per Annum</p>
+                                <p><strong>Job Type :</strong> Permanent</p>
+                                <p><strong>Location of Job :</strong> Nottingham/Hybrid</p>
+                                <br />
+                                <p><strong>Company background:</strong></p>
+                                <p>Repa is a global IT services company focusing on Enterprise solutions and integration services consulting (SAP), software development, product solutions, various testing solutions and IT consulting services offering to enterprises worldwide.</p>
+                                <br />
+                                <p>Repa specializes in the rapid development of bespoke software systems for start-up and fast moving businesses. We work closely with clients to develop business ideas and turn them into working software. Modern businesses recognize the need for continuous improvements rather than large, discrete projects.</p>
+                                <br />
+                                <h4>Detailed Job Description:</h4>
+
+                                <p><strong>Summary of role:</strong></p>
+                                <p>Software developers who are excited about solving new problems. As a developer, work across the technical landscape from front & backend (HTML5 / JS / CSS) to architectural and business logic in various environments including Java/ .net / Oracle/ SQL.</p>
+                                <p>We are looking for someone with a proven record of experience building scalable web applications and a vision for developing the technical products of the future.</p>
+                                <br />
+                                <p><strong>Skills required:</strong></p>
+
+                                <div className="services__details-left-content-list">
+                                    <div className="row">
+                                        <div className="col-sm-12 sm-mb-15">
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Object-oriented best practices and software designs patterns.</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Software architecture for websites, back office transactional systems and APIs (REST)</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Experience with Agile teams and working in sprint cycles</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Deploy and maintain apache and/or nginx based web applications</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Experience in deploying web portals using JavaScript, HTML5</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Assist with end-user support and Web application support</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Define and recommend policies and procedures for system and services use</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Experience with shell scripting in one or more of the following languages: Bash, Python, Ruby, Perl, and PHP.</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Strong software troubleshooting skills</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Understanding of / Experience with DevOps a plus</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Willingness to research, learn and adapt, attend trainings</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Understanding and working knowledge of SDLC and its application</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Strong experience in testing web applications and its functions to the maximum level.</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Nice to have knowledge of developing APIs for iOS/Android</div>
+                                            <div><i className="fa fa-check " style={{ color: 'green' }}></i> Hours - 37.5 to 40 hours per week.</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br />
+                                <p>An equal opportunities statement is recommended, potentially together with a specific welcome statement, for example, highlighting your desire for applications from individuals who are under-represented in the business.</p>
+                            </div>
+
+                            <div className="news__details-left rounded-2 mb-4 p-5" style={{ backgroundColor: 'rgba(235,235,235,.5)' }}>
+                                <div className="news__details-left-meta">
+                                    <h3 className="mt-20">Software Engineer</h3>
+                                </div>
+                                <p><strong>Job Title :</strong> Data Analyst</p>
+                                <p><strong>Salary :</strong> €33,000 - 38,000 Per Annum</p>
+                                <p><strong>Job Type :</strong> Permanent</p>
+                                <p><strong>Location of Job :</strong> Nottingham/Hybrid</p>
+                                <br />
+                                <p><strong>Company background:</strong></p>
+                                <p>Repa is a global IT services company focusing on Enterprise solutions and integration services consulting (SAP), software development, product solutions, various testing solutions and IT consulting services offering to enterprises worldwide.</p>
+                                <br />
+                                <p>Repa specializes in the rapid development of bespoke software systems for start-up and fast moving businesses. We work closely with clients to develop business ideas and turn them into working software. Modern businesses recognize the need for continuous improvements rather than large, discrete projects.</p>
+                                <br />
+                                <h4>Detailed Job Description:</h4>
+
+                                <p><strong>Summary of role:</strong></p>
+                                <p>This is an exciting opportunity to join our Software Engineering Practice as a Data Modeller, to work alongside our team and help build our Data Engineering capability. We are looking for someone with prior experience in Data Modelling, who is passionate about their work and can adapt to working within a versatile team, as well as having the ability to work closely with internal stakeholders and clients.</p>
+                                <p>Our team is our strongest asset; you will be an excellent team player who recognises the strength of the team as a whole and can find their role within the PNS team.</p>
+                                <br />
+                                <p><strong>Skills required:</strong></p>
+
+                                <div className="services__details-left-content-list">
+                                    <div className="row">
+                                        <div className="col-sm-12 sm-mb-15">
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Extensive experience of Data Modelling</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience in understanding data and metadata</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Applied experience of ETL tools (e.g. Informatica) and SQL/noSQL databases</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience with one of the following - Azure/AWS/GCP</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience with Business Glossary tools (e.g. Collibra)</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Knowledge of TDD/BDD</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience working in an Agile development environment</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Some experience in identity management and synchronisation</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience with Visio, Excel, Confluence, GDS Framework would be an advantage</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Experience with shell scripting in one or more of the following languages: Bash, Python, Ruby, Perl, PHP.</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Strong software troubleshooting skills</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Understanding of / Experience with DevOps a plus</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Willingness to research, learn and adapt, attend trainings</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Understanding and working knowledge of SDLC and its application</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Strong experience in testing web applications and its functions to the maximum level.</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Nice to have knowledge of developing APIs for iOS/Android</div>
+                                            <div><i className="fa fa-check" style={{ color: 'green' }}></i> Hours - 37.5 to 40 hours per week.</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br />
+                                <p>An equal opportunities statement is recommended, potentially together with a specific welcome statement, for example, highlighting your desire for applications from individuals who are under-represented in the business.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
